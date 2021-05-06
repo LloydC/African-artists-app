53607864500b76a1344b71a554e504afe45f1763
Lloyd Chambrier <lloydchambrier@gmail.com>
2021-05-06 11:14:21 +0000

    Added Nav component

--- src/App.js
+++ src/App.js
@@ -1,93 +1,19 @@
 import './App.css';
 import 'antd/dist/antd.css';
-import React, {useState} from 'react';
+import React from 'react';
 // import MapChart from './components/MapChart';
 import AfricaMap from './components/AfricaMap';
-import { AutoComplete, Input, Layout, Menu, Breadcrumb } from 'antd';
-const { Header, Content, Footer } = Layout;
+import Nav from './components/Nav';
+import { Layout, Breadcrumb } from 'antd';
+const { Content, Footer } = Layout;
 
-const getRandomInt = (max, min = 0) => {return Math.floor(Math.random() * (max - min + 1)) + min;} // eslint-disable-line no-mixed-operators
-
-const searchResult = (query) =>
-  new Array(getRandomInt(5))
-    .join('.')
-    .split('.')
-    .map((_, idx) => {
-      const category = `${query}${idx}`;
-      return {
-        value: category,
-        label: (
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-            }}
-          >
-            <span>
-              Found {query} on{' '}
-              <a
-                href={`https://s.taobao.com/search?q=${query}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {category}
-              </a>
-            </span>
-            <span>{getRandomInt(200, 100)} results</span>
-          </div>
-        ),
-      };
-    });
 
 function App() {
-  const [options, setOptions] = useState([]);
-
-  const handleSearch = (value) => {
-    setOptions(value ? searchResult(value) : []);
-  };
-
-  const onSelect = (value) => {
-    console.log('onSelect', value);
-  };
-
   return (
     <div className="App">
       <Layout>
-        <Header style={{ position: 'fixed', zIndex: 1, width: '100%', display: 'flex', justifyContent: 'space-between' }}>
-          <div className="logo" />
-          <AutoComplete
-              dropdownMatchSelectWidth={252}
-              style={{
-                width: 300,
-                display: 'flex',
-                alignItems: 'center'
-              }}
-              options={options}
-              onSelect={onSelect}
-              onSearch={handleSearch}
-            >
-                <Input.Search placeholder="Search for artists..." enterButton />
-            </AutoComplete>
-          <Menu theme="dark" mode="horizontal">
-          {/* <Menu.Item key="0">
-              <AutoComplete
-              dropdownMatchSelectWidth={252}
-              style={{
-                width: 300,
-                display: 'flex',
-              }}
-              options={options}
-              onSelect={onSelect}
-              onSearch={handleSearch}
-            >
-                <Input.Search size="large" placeholder="input here" enterButton />
-            </AutoComplete>
-            </Menu.Item> */}
-            <Menu.Item key="1">Songs</Menu.Item>
-            <Menu.Item key="2">Artists</Menu.Item>
-            <Menu.Item key="3">Playlists</Menu.Item>
-          </Menu>
-        </Header>
+        <Nav isLoggedIn={false}/>
+
         <Content className="site-layout"style={{ padding: '0 50px'}}>
           <Breadcrumb style={{ margin: '16px 0' }}>
             <Breadcrumb.Item>Home</Breadcrumb.Item>
--- src/components/AfricaMap.js
+++ src/components/AfricaMap.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { VectorMap } from '@south-paw/react-vector-maps';
+import styled from 'styled-components'
 import africa from '../assets/africa.json';
 
 const style = { margin: '1rem auto'};
@@ -16,10 +17,52 @@ const Map = () =>{
         onBlur: ({ target }) => setFocused('None'),
         onClick: ({ target }) => setClicked(target.attributes.name.value),
       };
+    
+      const Map = styled.div`
+      margin: 1rem auto;
+    
+      svg {
+        stroke: #fff;
+    
+        // All layers are just path elements
+        path {
+          fill: #1890ff;
+          cursor: pointer;
+          outline: none;
+    
+          // When a layer is hovered
+          &:hover {
+            fill: #001529;
+          }
+    
+          // When a layer is focused.
+          &:focus {
+            fill: #001529;
+          }
+    
+          // When a layer is 'checked' (via checkedLayers prop).
+          &[aria-checked='true'] {
+            fill: rgba(56,43,168,1);
+          }
+    
+          // When a layer is 'selected' (via currentLayers prop).
+          &[aria-current='true'] {
+            fill: rgba(56,43,168,0.83);
+          }
+    
+          // You can also highlight a specific layer via it's id
+          &[id="nz-can"] {
+            fill: rgba(56,43,168,0.6);
+          }
+        }
+      }
+    `;
 
     return (
         <div style={style}>
-          <VectorMap {...africa} layerProps={layerProps} />
+          <Map>
+            <VectorMap {...africa} layerProps={layerProps} />
+          </Map>
           <hr />
           <p>Hovered: {hovered && <code>{hovered}</code>}</p>
           <p>Focused: {focused && <code>{focused}</code>}</p>
